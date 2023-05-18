abf6633b6c8e2b2fdc652631db6a9195bc491c91
appdevel0293 <appdevel.0293@gmail.com>
2023-05-18 10:14:46 -0400

    first commit

--- src/verify.js
+++ src/verify.js
@@ -10,6 +10,7 @@ function Verify(){
     const index = href.lastIndexOf('/verify');
     const nameTemp = href.slice(0, index);
     const name = nameTemp.replaceAll('/','').replaceAll('.','');
+    console.log(name);
     const databaseRef = ref(db, `/${name}`);
     const [step, setStep] = useState('');
 
